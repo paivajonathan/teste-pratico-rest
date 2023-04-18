03ddff3bfb1dd7355f6fcd04500a097a59947d89
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-18 19:12:24 -0300

    configured model endereco

--- endereco/admin.py
+++ endereco/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from endereco.models import Endereco
 
-# Register your models here.
+
+admin.site.register(Endereco)
--- endereco/models.py
+++ endereco/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Endereco(models.Model):
+    vLogradouro = models.CharField(80)
+    vComplemento = models.CharField(50)
+    vBairro = models.CharField(40)
+    vNumero = models.CharField(10)
+    cUF = models.CharField(2)
+
+    def __str__(self):
+        return f'Logradouro: {self.vLogradouro}, Complemento: {self.vComplemento}, Bairro: {self.vBairro}, Numero: {self.vNumero}, UF: {self.cUF}'
