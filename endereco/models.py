161802c4fbb11515e9ac7fe68de467fcc5d9150d
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-18 19:24:16 -0300

    corrected endereco models.py

--- endereco/models.py
+++ endereco/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Endereco(models.Model):
-    vLogradouro = models.CharField(80)
-    vComplemento = models.CharField(50)
-    vBairro = models.CharField(40)
-    vNumero = models.CharField(10)
-    cUF = models.CharField(2)
+    vLogradouro = models.CharField(max_length=80)
+    vComplemento = models.CharField(max_length=50)
+    vBairro = models.CharField(max_length=40)
+    vNumero = models.CharField(max_length=10)
+    cUF = models.CharField(max_length=2)
 
     def __str__(self):
         return f'Logradouro: {self.vLogradouro}, Complemento: {self.vComplemento}, Bairro: {self.vBairro}, Numero: {self.vNumero}, UF: {self.cUF}'
