11d5a1efb45bbe4ef3837161d633cfb99e2c2cf2
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-18 21:30:02 -0300

    configured admin.py to register models

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from empresa.models import Empresa
 
-# Register your models here.
+admin.site.register(Empresa)
--- veiculo/admin.py
+++ veiculo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from veiculo.models import Veiculo
 
-# Register your models here.
+
+admin.site.register(Veiculo)
