5e386f76bd88c3bbcca4ee1e22b189dbb8237170
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-19 00:48:40 -0300

    added veiculo pagination resources

--- veiculo/api/viewsets.py
+++ veiculo/api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from veiculo.models import Veiculo
 from veiculo.api.serializers import VeiculoSerializer
+from rest_framework.pagination import PageNumberPagination
 
 
 class VeiculoViewSet(ModelViewSet):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
+    pagination_class = PageNumberPagination
