64ca02fd5da7bcbcdc1c3e75f30134deab964786
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-18 21:23:54 -0300

    configure veiculo models.py

--- veiculo/models.py
+++ veiculo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from empresa.models import Empresa
 
-# Create your models here.
+
+class Veiculo(models.Model):
+    vDesc_veiculo = models.CharField(max_length=35)
+    vPlaca = models.CharField(max_length=7)
+    nValor = models.DecimalField(max_digits=15, decimal_places=3)
+    iCod_empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Placa: {self.vPlaca}'
