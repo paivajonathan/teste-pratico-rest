11e65f9f3900506c0e0e01b842aa301b6678af08
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-19 00:48:02 -0300

    added empresa pagination resources

--- empresa/api/viewsets.py
+++ empresa/api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from empresa.models import Empresa
 from empresa.api.serializers import EmpresaSerializer
+from rest_framework.pagination import PageNumberPagination
 
 
 class EmpresaViewSet(ModelViewSet):
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
+    pagination_class = PageNumberPagination
