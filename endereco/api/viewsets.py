b3cdde381907819fd0d43d7d38268a71269c3e59
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-19 00:46:52 -0300

    added endereco pagination resources

--- endereco/api/viewsets.py
+++ endereco/api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from endereco.models import Endereco
 from endereco.api.serializers import EnderecoSerializer
+from rest_framework.pagination import PageNumberPagination
 
 
 class EnderecoViewSet(ModelViewSet):
     queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
+    pagination_class = PageNumberPagination
