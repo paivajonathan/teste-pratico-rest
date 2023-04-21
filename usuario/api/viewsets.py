30e16e54436fdf7d19d0585c7097bd79cc7aeb3f
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-21 19:49:14 -0300

    creating tests for usuario endpoints

--- usuario/api/viewsets.py
+++ usuario/api/viewsets.py
@@ -13,10 +13,10 @@ class UsuarioCreateView(CreateAPIView):
     permission_classes = [AllowAny]
     allowed_methods = ['POST']
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
             self.perform_create(serializer)
-            return redirect('/login/?next=/')
+            return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- usuario/tests.py
+++ usuario/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
 
-# Create your tests here.
+
+class UsuarioViewSetTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+    
+    def test_create_usuario(self):
+        data = {
+            "username": "UsuarioA",
+            "password": "A@123"
+        }
+        response = self.client.post('/registrar/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_authenticate_usuario(self):
+        data = {
+            "username": "UsuarioA",
+            "password": "A@123"
+        }
+        response = self.client.post('/login/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
