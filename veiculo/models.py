f244c8409c331bdbc80f998a8e84b7ebbbf2404e
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-23 16:38:40 -0300

    added veiculo model validations

--- veiculo/models.py
+++ veiculo/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from empresa.models import Empresa
 
 
 class Veiculo(models.Model):
     vDesc_veiculo = models.CharField(max_length=35)
-    vPlaca = models.CharField(max_length=7)
+    vPlaca = models.CharField(max_length=7, validators=[RegexValidator(regex=r'^[A-Z]{3}\d[A-Z0-9]{2}\d$', message='A placa deve seguir o formato LLLNLNN.')])
     nValor = models.DecimalField(max_digits=15, decimal_places=3)
     iCod_empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
 
