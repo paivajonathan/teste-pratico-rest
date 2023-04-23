7cabf74d1de55d23eaec2caf5c095f7080edb7a4
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-23 16:00:27 -0300

    fixed endereco model validations

--- endereco/models.py
+++ endereco/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.core.validators import RegexValidator, MinLengthValidator
+from django.core.validators import RegexValidator
 
 
 class Endereco(models.Model):
     vLogradouro = models.CharField(max_length=80)
     vComplemento = models.CharField(max_length=50)
     vBairro = models.CharField(max_length=40)
-    vNumero = models.CharField(max_length=10, validators=[RegexValidator(r'^[0-9]*$', 'Somente números são permitidos.')])
-    cUF = models.CharField(max_length=2, validators=[MinLengthValidator(2, 'Este campo deve ter 2 caracteres.')])
+    vNumero = models.CharField(max_length=10, validators=[RegexValidator(regex=r'^[0-9]*$', message='Somente números são permitidos.')])
+    cUF = models.CharField(max_length=2, validators=[RegexValidator(regex=r'^[A-Z]{2}$', message='O campo cUF deve ter exatamente dois caracteres maiúsculos.')])
 
     def __str__(self):
         return f'Logradouro: {self.vLogradouro}, Complemento: {self.vComplemento}, Bairro: {self.vBairro}, Numero: {self.vNumero}, UF: {self.cUF}'
