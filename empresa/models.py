15b75255846d3adf3fe13ad8088c2af51386333f
Jonathan Paiva <jonathanapaiva@gmail.com>
2023-04-23 16:13:24 -0300

    added empresa model validations

--- empresa/models.py
+++ empresa/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, EmailValidator
 from endereco.models import Endereco
 
 
@@ -7,9 +7,9 @@ class Empresa(models.Model):
     vNome_fantasia = models.CharField(max_length=80)
     vNome_razao_social = models.CharField(max_length=80)
     vCnpj = models.CharField(max_length=14, validators=[RegexValidator(r'^\d{14}$', 'O CNPJ deve conter 14 dígitos.')])
-    vEmail = models.CharField(max_length=40)
+    vEmail = models.CharField(max_length=40, validators=[EmailValidator(message='Digite um email válido.')])
     vObservacao = models.CharField(max_length=200)
-    iCod_endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE)
+    iCod_endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE, error_messages={ 'unique': 'Endereço já cadastrado.' })
 
     def __str__(self):
         return f'Nome fantasia: {self.vNome_fantasia}, Razao social: {self.vNome_razao_social}'
